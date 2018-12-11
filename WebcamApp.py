28a9ff89051cc5e71e1ab33bf3b2fe170d22d237
Dwi Ana Ambar Rofiqoh <45792203+dwianaambarr@users.noreply.github.com>
2018-12-11 22:43:53 +0700

    Update WebcamApp.py

--- WebcamApp.py
+++ WebcamApp.py
@@ -10,8 +10,10 @@ print('\t\t   *Welcome to the Simple Webcam Application*\n\n'
       3*' ' + 'Press 3 to Save Photo in Blur Filter\n',
       3*' ' + 'Press 4 to Save Video Recording and Quit the Application\n')
 
-#Setting for Image cap and Upper Value
-i = 0
+#Setting for Image Cap and Upper Value
+a = 0
+b = 0
+c = 0
 upper = [0,0,0]
 
 #Find HSV Green Value 
@@ -67,20 +69,20 @@ while(cap.isOpened()):
     k = cv2.waitKey(1) & 0xFF
     if k == 49: #ord 1
         #Take a Photo
-        i += 1
-        colorName = "Result {}.png".format(i)
+        a += 1
+        colorName = "Result {}.png".format(a)
         #Save Photo
         cv2.imwrite(colorName, frame)
     elif k == 50: #ord 2           
         gray = cv2.cvtColor( frame, cv2.COLOR_BGR2GRAY )
-        i += 1
-        grayName = "Grayscale {}.png".format(i)
+        b += 1
+        grayName = "Grayscale {}.png".format(b)
         #Save Photo
         cv2.imwrite(grayName, gray)
     elif k == 51 : #ord 3
         blur = cv2.blur(frame,(7,7))
-        i += 1
-        blurName = "Blur {}.png".format(i)
+        c += 1
+        blurName = "Blur {}.png".format(c)
         cv2.imwrite(blurName, blur)
     elif k == 52 : #ord 4
         #Quit
